865714d0e3f216c2f6b786af7e2f99791b35a79f
Harisankar <harisankar@uniqnex360.com>
2025-09-25 10:39:26 +0530

    REDIS_URL:Updated

--- PIM/settings.py
+++ PIM/settings.py
@@ -59,7 +59,7 @@ SESSION_CACHE_ALIAS = "default"
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://:foobaredUniqnex@127.0.0.1:6379/1',
+        'LOCATION': os.getenv('REDIS_URL'),
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
